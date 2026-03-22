7a3472a3ce70019a1d2cd2379ec11c02fb345b34
krishna250404 <goswamikrishna2544@gmail.com>
2026-03-22 22:14:19 +0530

    fixed routes mismatch

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,20 +4,20 @@ const API = axios.create({
   baseURL: "https://fitness-tracker-gbix.onrender.com",
 });
 
-export const UserSignUp = async (data) => API.post("/user/signup", data);
-export const UserSignIn = async (data) => API.post("/user/signin", data);
+export const UserSignUp = async (data) => API.post("api/user/signup", data);
+export const UserSignIn = async (data) => API.post("api/user/signin", data);
 
 export const getDashboardDetails = async (token) =>
-  API.get("/user/dashboard", {
+  API.get("api/user/dashboard", {
     headers: { Authorization: `Bearer ${token}` },
   });
 
 export const getWorkouts = async (token, date) =>
-  await API.get(`/user/workout${date}`, {
+  await API.get(`api/user/workout${date}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
 export const addWorkout = async (token, data) =>
-  await API.post(`/user/workout`, data, {
+  await API.post(`api/user/workout`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
